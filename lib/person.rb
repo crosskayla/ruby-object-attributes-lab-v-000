cac3245dc385304d1dc17a52f69d3a871b5a0b95
Kayla Cross <kac288@georgetown.edu>
2018-02-05 03:46:54 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
 class Person
   def name=(person_name)
-    @this_persons_name = person_name
+    @name = person_name
   end
   def name
-    @this_persons_name
+    @name
   end
   def job=(person_job)
-    @this_persons_job = person_job
+    @job = person_job
   end
   def job
-    @this_persons_job
+    @job
   end
 end
